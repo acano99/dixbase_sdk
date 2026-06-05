3b86c2875af58724badbe4d31e51931e1a987402
abrahan <abrahan.jcano99@gmail.com>
2026-06-05 13:09:25 +0200

    correccion en los endpoints

--- lib/src/client/endpoints.dart
+++ lib/src/client/endpoints.dart
@@ -1,5 +1,5 @@
 class Endpoints {
-  static const String baseUrl = 'https://pay.dixbase.com/api/v1';
-  static const String listCurrencies = '/currencies';
+  static const String baseUrl = 'https://pay.dixbase.com/api/v1/';
+  static const String listCurrencies = '/currency';
   static const String getBusiness = '/business';
 }
