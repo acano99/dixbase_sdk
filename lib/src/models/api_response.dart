54231f87df9de1884ed94daed746c47b88137767
abrahan <abrahan.jcano99@gmail.com>
2026-06-05 13:45:03 +0200

    correccion en el modelo de datos

--- lib/src/models/api_response.dart
+++ lib/src/models/api_response.dart
@@ -1,9 +1,11 @@
 class DixbaseListResponse<T> {
+  final String url;
   final String object;
   final bool hasMore;
   final List<T> data;
 
   DixbaseListResponse({
+    required this.url,
     required this.object,
     required this.hasMore,
     required this.data,
@@ -14,6 +16,7 @@ class DixbaseListResponse<T> {
     T Function(Map<String, dynamic>) fromJsonT,
   ) {
     return DixbaseListResponse(
+      url: json['url'] as String,
       object: json['object'] as String,
       hasMore: json['has_more'] as bool,
       data: (json['data'] as List)
