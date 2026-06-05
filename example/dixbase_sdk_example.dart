e1364ec5ebde6a449241b6c88ce951bb7c4e9183
abrahan <abrahan.jcano99@gmail.com>
2026-06-05 06:04:02 +0200

    creacion del proyecto

---
+++ example/dixbase_sdk_example.dart
@@ -0,0 +1,6 @@
+import 'package:dixbase_sdk/dixbase_sdk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dixbase_sdk_test.dart
@@ -0,0 +1,16 @@
+import 'package:dixbase_sdk/dixbase_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
